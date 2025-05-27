aa7d178d1c27d583f7576960a543bffef7b18b6c
Maksym Panasovskyi <ie.panasovskyi@gmail.com>
2025-05-27 13:42:11 +0100

    s

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,34 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={activeTabId === tab.id ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                onClick={() => {
+                  if (tab.id === activeTabId) return;
+                  onTabSelected(tab.id);
+                }}
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {!activeTabId
+          ? tabs[0].content
+          : tabs.map(tab => (activeTabId === tab.id ? tab.content : ''))}
+      </div>
+    </div>
+  );
+};
